58ca9d081582af70f12f484995c547efb86c563b
Icappucino <93023359+Icappucino@users.noreply.github.com>
2023-06-16 22:00:52 +0700

    Update server.js add JWT

--- src/server.js
+++ src/server.js
@@ -1,36 +1,59 @@
-const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
+const Hapi = require('@hapi/hapi');
 const jwt = require('jsonwebtoken');
 
 const server = Hapi.server({
   port: 3000,
-  host: 'localhost',
+  host: 'woven-edge-381212:asia-southeast2:jasain',
 });
 
-const validate = async (decoded, request, h) => {
-  // Verifikasi token JWT
-  // Misalnya, Anda dapat melakukan pengecekan ke database apakah user dengan ID tersebut ada
-  // Contoh implementasi sederhana hanya melakukan pengecekan ID
-  if (decoded.id) {
-    return { isValid: true };
-  } else {
-    return { isValid: false };
+// Fungsi untuk membuat token JWT
+const generateToken = (userId) => {
+  const payload = {
+    sub: userId,
+    // Tambahkan klaim lain yang diperlukan dalam konteks aplikasi
+  };
+
+  // Ganti 'your-secret-key' dengan kunci rahasia yang sesuai
+  const secretKey = 'your-secret-key';
+
+  // Buat token JWT dengan menggunakan jwt.sign()
+  const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
+
+  return token;
+};
+
+// Contoh fungsi login
+const login = (request, h) => {
+  // Proses autentikasi pengguna
+  const userId = 123; // Contoh ID pengguna yang berhasil diautentikasi
+
+  // Generate token JWT
+  const token = generateToken(userId);
+
+  return {
+    token,
+  };
+};
+
+// Contoh penggunaan fungsi login
+const handleLogin = async (request, h) => {
+  try {
+    const response = await login(request, h);
+    return response;
+  } catch (error) {
+    console.error(error);
+    return h.response('Failed to login').code(500);
   }
 };
 
+// Menjalankan server Hapi.js
 const init = async () => {
-  await server.register(require('@hapi/cookie'));
-  await server.register(require('hapi-auth-jwt2'));
-
-  server.auth.strategy('jwt', 'jwt', {
-    key: 'your-secret-key',
-    validate,
-    verifyOptions: { algorithms: ['HS256'] },
+  server.route({
+    method: 'POST',
+    path: '/login',
+    handler: handleLogin,
   });
-
-  server.auth.default('jwt');
-
-  server.route(routes);
 
   await server.start();
   console.log('Server running on %s', server.info.uri);
